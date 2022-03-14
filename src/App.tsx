ff1c07519add7684c5b15ba535ef7a82f3994871
Tim Marwick <triyuga@gmail.com>
2022-03-14 21:02:12 +1000

    Add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => (
     <BrowserRouter>
         {/* Routes nest inside one another. Nested route paths build upon parent route paths, and nested route elements render inside parent route elements. See the note about <Outlet> below. */}
         <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="/sea-of-fallen-stars/" element={<Layout />}>
                 <Route index element={<SeaOfFallenStars />} />
                 <Route path="about" element={<About />} />
                 <Route path="dashboard" element={<Dashboard />} />
